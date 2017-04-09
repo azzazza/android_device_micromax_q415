73bd58788e13e321e967fcacd4847193d42222ec
Ubuntu <assusdan@srtkernels.srtkernels1942.x9.internal.cloudapp.net>
2017-04-09 16:40:21 +0000

    ac50ehe: sesnors: Use prebuilt sensors HAL
    Change-Id: Ie1b91f2cceefc5d857c155db94ae1b457587e189

--- y560.mk
+++ y560.mk
@@ -29,11 +29,6 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
-# Sensors
-PRODUCT_PACKAGES += \
-    sensord \
-    sensors.msm8909
-
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
