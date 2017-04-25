ec321071e7d29e7089abc9602636031a1320df09
azzazza <kostik-x1@yandex.ru>
2017-04-25 05:59:58 +0300

    q415_adaptable

--- lineage.mk
+++ lineage.mk
@@ -1,19 +1,19 @@
 # Boot animation
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 480
+TARGET_SCREEN_HEIGHT := 854
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/archos/ac50ehe/ac50ehe.mk)
+$(call inherit-product, device/micromax/q415/q415.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := ac50ehe
-PRODUCT_NAME := lineage_ac50ehe
-PRODUCT_BRAND := ARCHOS
-PRODUCT_MODEL := 50e_Helium
-PRODUCT_MANUFACTURER := ARCHOS
-PRODUCT_RELEASE_NAME := 50e_Helium
+PRODUCT_DEVICE := q415
+PRODUCT_NAME := lineage_q415
+PRODUCT_BRAND := MICROMAX
+PRODUCT_MODEL := Pace_4G
+PRODUCT_MANUFACTURER := MICROMAX
+PRODUCT_RELEASE_NAME := Pace_4G
 
-PRODUCT_GMS_CLIENTID_BASE := android-archos
+PRODUCT_GMS_CLIENTID_BASE := android-micromax
